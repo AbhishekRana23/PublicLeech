ccdf90ec8554b3d6d41e97292d82c7063df78b0d
AbhishekRana23 <40822125+AbhishekRana23@users.noreply.github.com>
2019-11-22 18:34:14 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from tobrot.sample_config import Config
-
-class Config(Config):
-  TG_BOT_TOKEN = "941965737:AAF7S_9Tytvn7iUq11qaShVYzDRfInYdQqE"
-  APP_ID = 1067319
-  API_HASH = "147745612b97af716648f754d6232a1e"
-  AUTH_CHANNEL = -1001239130697
-  PROCESS_MAX_TIMEOUT = 3600
